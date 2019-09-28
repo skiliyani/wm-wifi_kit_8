b3cbe83935d0b3a28964409a783fd2dc9bdf2da4
Sajeesh Kiliyani <skiliyani@binaryfountain.com>
2019-09-28 17:26:04 +0530

    Initial commit with WiFi connectivity

---
+++ wm-wifi_kit_8.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+
+const char* ssid = "SAYANI_IOT";
+const char* password = "00001111";
+
+void setup_wifi() {
+
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+
+}
+
+void setup(void) {
+  Serial.begin(115200);
+
+  setup_wifi();
+}
+
+
+void loop(void) {
+ 
+}
